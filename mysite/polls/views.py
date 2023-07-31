b9b374305f2b5a1252041cbe2c3b748d8ade835d
Your Name <you@example.com>
2023-07-31 18:39:45 +0000

    g

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
-from django.http import HttpResponse
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-def indextwo(request):
-    return HttpResponse("Lord Sauron Is Here")
+
+
+
+
+
+# from django.shortcuts import render
+
+# # Create your views here.
+# from django.http import HttpResponse
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def indextwo(request):
+#     return HttpResponse("Lord Sauron Is Here")
