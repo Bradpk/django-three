e620144ca02a124a0dedd084fc8311816be1f677
Your Name <you@example.com>
2023-07-31 18:42:22 +0000

    g

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from polls import views
 
-from . import views
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('next/', views.indextwo, name='indextwo'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+
+
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('next/', views.indextwo, name='indextwo'),
+# ]
