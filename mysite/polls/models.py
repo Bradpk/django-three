605541dda692e2f3a44b7daf9efb2abfa38492c6
Your Name <you@example.com>
2023-07-31 18:29:18 +0000

    g

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Topping(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False, unique=True)
+
+class Pizza(models.Model):
+    size = models.IntegerField()
+    toppings = models.ManyToManyField(Topping)
